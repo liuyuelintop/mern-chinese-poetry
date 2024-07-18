0831e96f23b27c1b403ca5c5412347fdce74662b
liuyuelintop <liuyuelintop@gmail.com>
2024-07-18 22:43:56 +1000

    Edit Server/index.js to prepare for deployment

--- server/index.js
+++ server/index.js
@@ -5,25 +5,28 @@ import poetryRoutes from "./routes/poetry.route.js";
 import shijingRoutes from "./routes/shijing.route.js";
 import searchRoutes from "./routes/search.route.js"; // 确保路径正确
 import connectMongoDB from "./db/connectMongoDB.js";
+import path from "path";
 dotenv.config();
 
+const __dirname = path.resolve();
+
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-const corsOptions = {
-  origin: "http://localhost:5173", // 允许的前端地址
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
-};
+app.use(cors());
 
-// 中间件
-app.use(cors(corsOptions));
 app.use(express.json());
 
 // 路由
 app.use("/api/poems", poetryRoutes);
 app.use("/api/shijing", shijingRoutes);
 app.use("/api/search", searchRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist"))); // vite-->dist || npx create -->build
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}\n`);
